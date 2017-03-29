2c7d8ab862a38f5758e9be3db79c087449e93a8f
Federico Ficarelli <federico.ficarelli@gmail.com>
2017-03-29 15:34:38 +0200

    add safe decondig of data payloads and basic logging

--- trac.py
+++ trac.py
@@ -1,12 +1,37 @@
 # -*- coding: utf-8 -*-
 
 import ssl
+import logging
 
 import six
 from six.moves import xmlrpc_client as xmlrpc
 
 
+LOG = logging.getLogger(__name__)
+
+
+def _safe_encode(data, encoding='base64'):
+    try:
+        six.u(data.decode(encoding))
+    except Exception as e:
+        LOG.exception('error while decoding data from %s', encoding)
+        return six.u(str(e))
+
+
+def _authors_collect(wiki=None, tickets=None):
+    wiki = wiki or []
+    tickets = tickets or []
+    return list(set(
+        [page['attributes']['author'] for page in six.itervalues(wiki)] + \
+        [ticket['attributes']['reporter'] for ticket in six.itervalues(tickets)] + \
+        [ticket['attributes']['owner'] for ticket in six.itervalues(tickets)] + \
+        [change['author'] for ticket in six.itervalues(tickets) for change in ticket['changelog']]
+        # TODO crawl wiki attachments for additional authors
+    ))
+
+
 def ticket_get_changelog(ticket_id, source):
+    LOG.debug('ticket_get_changelog of ticket %s', ticket_id)
     return [
         {
             'time': c[0],
@@ -21,6 +46,7 @@ def ticket_get_changelog(ticket_id, source):
 
 
 def ticket_get_attachments(ticket_id, source):
+    LOG.debug('ticket_get_attachments of ticket %s', ticket_id)
     return {
         meta[0]: {
             'attributes': {
@@ -30,13 +56,14 @@ def ticket_get_attachments(ticket_id, source):
                 'time': meta[3],
                 'author': meta[4],
             },
-            'data': six.u(source.ticket.getAttachment(ticket_id, meta[0]).data.decode('base64'))
+            'data': _safe_encode(source.ticket.getAttachment(ticket_id, meta[0]).data)
         }
         for meta in source.ticket.listAttachments(ticket_id)
     }
 
 
 def ticket_get_all(source):
+    LOG.debug('ticket_get_all')
     return {
         tid: {
             'attributes': attrs,
@@ -49,6 +76,7 @@ def ticket_get_all(source):
 
 
 def milestone_get_all(source):
+    LOG.debug('milestone_get_all')
     return {
         milestone: source.ticket.milestone.get(milestone)
             for milestone in milestone_get_all_names(source)
@@ -56,42 +84,51 @@ def milestone_get_all(source):
 
 
 def milestone_get(milestone_name, source):
+    LOG.debug('milestone_get of milestone %s', milestone_name)
     return source.ticket.milestone.get(milestone_name)
 
 
 def milestone_get_all_names(source):
+    LOG.debug('milestone_get_all_names')
     return list(source.ticket.milestone.getAll())
 
 
 def wiki_get_all_pages(source, exclude_authors=None, exclude_trac_pages=True):
+    LOG.debug('wiki_get_all_pages')
     exclude_authors = set(exclude_authors or [])
     if exclude_trac_pages:
         exclude_authors.add('trac')
+    LOG.debug('wiki_get_all_pages is retrieving metadata for all pages')
     pages = {
         name: {'attributes': source.wiki.getPageInfo(name)}
             for name in source.wiki.getAllPages()
     }
     if exclude_authors:
+        LOG.debug('wiki_get_all_pages is excluding authors: %s', exclude_authors)
         pages = {
             k: v for k, v in six.iteritems(pages)
                 if v['attributes']['author'] not in exclude_authors
         }
     for pagename, pagedict in six.iteritems(pages):
+        LOG.debug('wiki_get_all_pages is retrieving contents for wiki page %s', pagename)
         pagedict['page'] = source.wiki.getPage(pagename)
+        LOG.debug('wiki_get_all_pages is retrieving attachments for wiki page %s', pagename)
         pagedict['attachments'] = {
-            filename: six.u(source.wiki.getAttachment(filename).data.decode('base64'))
+            filename: _safe_encode(source.wiki.getAttachment(filename).data)
                 for filename in source.wiki.listAttachments(pagename)
         }
     return pages
 
 
 def project_get(source, collect_authors=True):
+    LOG.debug('project_get')
     project = {
         'wiki': wiki_get_all_pages(source),
         'tickets': ticket_get_all(source),
         'milestones': milestone_get_all(source),
     }
     if collect_authors:
+        LOG.debug('project_get is collecting authors from project')
         project['authors'] = _authors_collect(wiki=project['wiki'], tickets=project['tickets'])
     return project
 
@@ -100,18 +137,6 @@ def authors_get(source, from_wiki=True, from_tickets=True):
     wiki = wiki_get_all_pages(source) if from_wiki else None
     tickets = ticket_get_all(source) if from_tickets else None
     return _authors_collect(wiki=wiki, tickets=tickets)
-
-
-def _authors_collect(wiki=None, tickets=None):
-    wiki = wiki or []
-    tickets = tickets or []
-    return list(set(
-        [page['attributes']['author'] for page in six.itervalues(wiki)] + \
-        [ticket['attributes']['reporter'] for ticket in six.itervalues(tickets)] + \
-        [ticket['attributes']['owner'] for ticket in six.itervalues(tickets)] + \
-        [change['author'] for ticket in six.itervalues(tickets) for change in ticket['changelog']]
-        # TODO crawl wiki attachments for additional authors
-    ))
 
 
 def connect(url, encoding='UTF-8', use_datetime=True, ssl_verify=True):
